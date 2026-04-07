e5ca4613d7043bd1c1b572b8b1979b7cd2cc3f75
W-Maxim <maxim.wenkemann@outlook.de>
2026-04-07 15:07:31 +0200

    erste rohfassung vom authors command

--- src/bin/git_repo_analyzer.rs
+++ src/bin/git_repo_analyzer.rs
@@ -1,5 +1,23 @@
 use clap::{Parser, Subcommand};
 
-fn main() {
+#[derive(Parser)]
+#[command(name = "git-repo-analyzer")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
+#[derive(Subcommand)]
+enum Commands {
+    Authors,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Authors => {
+            println!("Authors command wird ausgeführt");
+        }
+    }
 }
